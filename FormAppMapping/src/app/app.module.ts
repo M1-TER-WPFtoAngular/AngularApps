4c1b9d8c92c19fb7f1e425a3011430ddbe3092b9
gaetanrm <gaetanrm@github.com>
2022-04-18 15:29:10 +0200

    mainwindow component generated

--- FormAppMapping/src/app/app.module.ts
+++ FormAppMapping/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Window1Component } from './window1/window1.component';
+import { MainwindowComponent } from './mainwindow/mainwindow.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Window1Component
+    Window1Component,
+    MainwindowComponent
   ],
   imports: [
     BrowserModule,
---
+++ FormAppMapping/src/app/mainwindow/mainwindow.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mainwindow',
+  templateUrl: './mainwindow.component.html',
+  styleUrls: ['./mainwindow.component.css']
+})
+export class MainwindowComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  public ajouter(sender : HTMLElement) : void { 
+  }
+
+  public supprimer(sender : HTMLElement) : void { 
+  }
+
+  public listView_Click() : void { 
+  }
+}
